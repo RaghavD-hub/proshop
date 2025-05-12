c68cb61d76453041766229fe6ae5311c316e8361
Raghav Dobriyal <dobriyalraghav10@gmail.com>
2025-05-12 22:47:04 +0530

    commit

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,9 +1,20 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <h1>Welocme to ProS hop</h1>
-   )
- }
- 
- export default App
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+      
+      <Header />
+      <main className='py-3'>
+        <Container>
+         <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default App;
