772cb7d0353b73f9bd52a17a3519ec4df6f18454
Raghav Dobriyal <dobriyalraghav10@gmail.com>
2025-05-28 13:08:04 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
  import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js';
 
 connectDB(); //connect to MangoDB
 
@@ -25,17 +26,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-/*app.get(`/api/products`, (req, res) => {
-  res.json(products);
-});
-
-app.get(`/api/products/:id`, (req, res) => {
-     const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});*/
 
 app.listen(port, () => console.log(`server running on port ${port}`));
